65dd6780ce0c14be487a8408bd5ca278aba689af
Amit Punj <punj.corp@gmail.com>
2018-10-17 21:55:29 +0530

    [Iteration 35] - [Amit] Application Installer Structure Changes
    This  check in has the changes related to installer structure which will be used to created installer

---
+++ assemble/app/pi_pos_app/bin/run.bat
@@ -0,0 +1,12 @@
+@ECHO OFF
+
+set PI_POS_HOME="PI_POS_T_HOME"
+
+set JAVA_HOME="PI_POS_T_HOME/utils/jre/"
+set PATH=%PATH%;%JAVA_HOME%/bin/;.;
+
+set PI_POS_APP_HOME=%PI_POS_HOME%/pi_pos_app/
+cd %PI_POS_APP_HOME%
+
+ECHO start PI-POS-APP
+start "PI-POS-APP" java -jar lib\pi_pos_app.jar
